5bf087141fb2e677b65010407c51ad31a7a51fb5
Muhammad-Ibrahim-Tayyab <ibrahim@ccript.com>
2024-02-06 17:20:14 +0500

    Updated Auths

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,8 +1,8 @@
 const bcrypt = require("bcrypt");
-const { User } = require("../models");
-const jwt = require("../utils/jwt")
+const jwt = require("../utils/jwt");
+const { User, School } = require("../models");
 
-const UserShouldNotPreExist = async(req, res, next) => {
+const UserShouldNotPreExist = async (req, res, next) => {
     try {
         const { email } = req.body;
         const isEmailRegisterd = await User.findOne({ where: { email } });
@@ -20,12 +20,12 @@ const UserShouldNotPreExist = async(req, res, next) => {
             .status(500)
             .json({ status: "error", message: "Internal Server Error" });
     }
-}
+};
 
-const UserShouldPreExist = async(req, res, next) => {
+const UserShouldPreExist = async (req, res, next) => {
     try {
         const { email, password } = req.body;
-        
+
         const user = await User.findOne({ where: { email } });
 
         if (!user || !bcrypt.compareSync(password, user.password)) {
@@ -42,18 +42,18 @@ const UserShouldPreExist = async(req, res, next) => {
             .status(500)
             .json({ status: "error", message: "Internal Server Error" });
     }
-}
+};
 
-const VerifyPassword = async(req, res, next) => {
+const VerifyPassword = async (req, res, next) => {
     try {
         const { password } = req.body;
-        
+
         const user = req.user;
 
         if (!bcrypt.compareSync(password, user.password)) {
-          return res
-            .status(401)
-            .json({ status: "error", message: "Invalid credentials" });
+            return res
+                .status(401)
+                .json({ status: "error", message: "Invalid credentials" });
         }
 
         return next();
@@ -62,31 +62,93 @@ const VerifyPassword = async(req, res, next) => {
             .status(500)
             .json({ status: "error", message: "Internal Server Error" });
     }
-}
+};
 
-const decodeAuthToken = async(req, res, next) => {
+const decodeAuthAndSetUser = async (req, res, next) => {
     try {
-        const authcookie = req.cookies.authcookie;
+        console.log("authcookie", authcookie)
+        console.log("req.authcookie", req.authcookie)
 
         const { email, userID } = jwt.verifyAccessToken(authcookie);
-    
-        // req.email = email;
-        // req.userID = userID;
+
+        req.email = email;
+        req.userID = userID;
 
         const user = await User.findOne({ where: { email } });
         req.user = user;
-    
+
         next();
     } catch (error) {
         return res
             .status(401)
             .json({ status: "error", message: "Invalid credentials" });
     }
-}
+};
+
+const decodeForgotPasswordAuth = async (req, res, next) => {
+    try {
+        const { email, userID } = jwt.verifyForgotPasswordToken(validationCookie);
+
+        req.email = email;
+        req.userID = userID;
+
+        const user = await User.findOne({ where: { email } });
+        req.user = user;
+
+        next();
+    } catch (error) {
+        return res
+            .status(401)
+            .json({ status: "error", message: "Invalid credentials" });
+    }
+};
+
+const deStructureBodyAndSetUser = async (req, res, next) => {
+    try {
+        const { email } = req.body;
+
+        const user = await User.findOne({ where: { email } });
+        
+        if (!user) {
+            return res
+            .status(401)
+            .json({ status: "error", message: "Email not registered" });
+        }
+        
+        req.user = user;
+
+        next();
+    } catch (error) {
+        return res
+            .status(401)
+            .json({ status: "error", message: "Invalid credentials" });
+    }
+};
+
+const setSchoolWithDecodedUser = async (req, res, next) => {
+    try {
+        const school = await School.findOne({ where: { createdBy: req.user.id } });
+
+        if (school === null) {
+            console.log("Not found!");
+        } 
+        else {
+            req.school = school;
+            next();
+        }
+    } catch (error) {
+        return res
+            .status(404)
+            .json({ status: "error", message: "School Not Found" });
+    }
+};
 
 module.exports = {
     UserShouldNotPreExist,
     UserShouldPreExist,
     VerifyPassword,
-    decodeAuthToken
-}
+    decodeAuthAndSetUser,
+    deStructureBodyAndSetUser,
+    setSchoolWithDecodedUser,
+    decodeForgotPasswordAuth
+};
