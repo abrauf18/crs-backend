e931fc82602e553efdee81b7ac6fa97fb45ee477
Muhammad-Ibrahim-Tayyab <ibrahim@ccript.com>
2024-02-07 10:15:04 +0500

    improved smaller middlewares

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -6,10 +6,10 @@ const router = express.Router();
 
 router.post("/signup", authMiddleware.UserShouldNotPreExist, authController.signup);
 router.post("/login", authMiddleware.UserShouldPreExist, authMiddleware.VerifyPassword, authController.login);
-router.post("/register-school", authMiddleware.decodeAuthAndSetUser, authController.createSchoolProfile);
-router.post("/invite-teachers", authMiddleware.decodeAuthAndSetUser, authMiddleware.setSchoolWithDecodedUser, authController.sendInviteToTeacher);
-router.post("/forgot-password", authMiddleware.deStructureBodyAndSetUser, authController.sendOTP);
-router.post("/verify-otp", authMiddleware.decodeForgotPasswordAuth, authController.verifyOTP);
-router.post("/reset-password", authMiddleware.decodeForgotPasswordAuth, authController.resetPassword);
+router.post("/register-school", authMiddleware.decodeLoginSignupCookie, authMiddleware.setUserUsingEmail, authController.createSchoolProfile);
+router.post("/invite-teachers", authMiddleware.decodeLoginSignupCookie, authMiddleware.setUserUsingEmail, authMiddleware.setSchoolUsingUserID, authController.sendInviteToTeacher);
+router.post("/forgot-password", authMiddleware.destructureBody, authMiddleware.setUserUsingEmail, authController.sendOTP);
+router.post("/verify-otp", authMiddleware.decodeForgotPasswordCookie, authMiddleware.setUserUsingEmail, authController.verifyOTP);
+router.post("/reset-password", authMiddleware.decodeForgotPasswordCookie, authMiddleware.setUserUsingEmail, authController.resetPassword);
 
 module.exports = router;
