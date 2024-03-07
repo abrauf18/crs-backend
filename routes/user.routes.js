91c85fa08020be35d6da28086cbd511e274daf5a
Muhammad-Ibrahim-Tayyab <158137112+Muhammad-Ibrahim-Tayyab@users.noreply.github.com>
2024-03-07 21:32:46 +0500

    feat: creates APIs for get and update profile with respective Joi middleware

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,9 +1,11 @@
 // auth.js
 const express = require("express");
+const userValidation = require("../middlewares/bodyValidators/user");
 const userController = require("../controllers/userController");
+const roleBasedAccess = require("../middlewares/roleBasedAccessControllers/index")
 
 const router = express.Router();
-router.get("/", userController.getAllUsers);
-router.get("/:id", userController.getUserById);
+router.get("/getUserProfile", userValidation.getUserProfile, roleBasedAccess.setUser, userController.getUserProfile)
+router.post("/updateUserProfile", userValidation.updateUserProfile, roleBasedAccess.setUser, userController.updateUserProfile)
 
 module.exports = router;
