55229aad72828be4d1e5b445985d2ce6738b2bbe
abrauf18 <abrauf.dev@gmail.com>
2024-03-27 16:29:22 +0500

    feat: add morgan to work alongside winston

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cookieParser = require("cookie-parser")
 const authRouter = require("./routes/auth-routes");
 const userRouter = require("./routes/user-routes");
 const schoolRouter = require("./routes/school-routes");
+const { logger, morganMiddleware } = require('./Logs/logger');
 const cors = require('cors');
 const db = require("./models");
 dotenv.config();
@@ -11,13 +12,16 @@ dotenv.config();
 db.sequelize
   .authenticate()
   .then(() => {
-    console.log("Connected to the database from sequelize");
+    logger.info("Connected to the database from sequelize");
   })
   .catch((error) => {
     console.error("Error connecting to the database from sequelize:", error);
   });
 
 const app = express();
+
+// Use morgan middleware for logging
+app.use(morganMiddleware);
 
 app.use(cors({origin: 'http://localhost:3000', credentials: true}));
 
@@ -37,13 +41,13 @@ const port = process.env.PORT || 3000;
 db.sequelize
   .sync()
   .then(() => {
-    console.log("Succesfully initialized DB");
+    logger.info("Succesfully initialized DB");
   })
   .catch((err) => {
-    console.log(err);
-    console.log("Error while connecting to the database from DB.sequelize");
+    logger.error(err);
+    logger.error("Error while connecting to the database from DB.sequelize");
   });
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  logger.info(`Server is running on port ${port}`);
 });
