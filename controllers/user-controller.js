2a35b7d8c81a8ca1353f9b4d88adfc188fdf2c88
abrauf18 <abrauf.dev@gmail.com>
2024-03-28 15:33:10 +0500

    feat: create get all users and update users endpoints

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,6 +1,6 @@
 const userService = require("../services/user-service.js");
 const { handleInternalServerError, handleSuccessResponse, handleErrorResponse } = require("../utils/response-handlers.js")
-const logger = require("../Logs/logger.js");
+const {logger} = require("../Logs/logger.js");
 
 const getUserProfile = async (req, res) => {
   try {
@@ -14,7 +14,7 @@ const getUserProfile = async (req, res) => {
     }
   }
   catch (error) {
-    logger.error(error);
+    logger.error(error.message);
     return handleInternalServerError(res);
   }
 };
@@ -37,7 +37,47 @@ const updateUserProfile = async (req, res) => {
     }
   }
   catch (error) {
-    logger.error(error);
+    logger.error(error.message);
+    return handleInternalServerError(res);
+  }
+};
+
+const getAllUsersProfile = async (req, res) => {
+  try {
+
+    const reply = await userService.getAllUsersProfile({user: req.user});
+
+    if (reply.code == 200) {
+      return handleSuccessResponse(res, 200, reply.data);
+    }
+    else {
+      return handleInternalServerError(res);
+    }
+  }
+  catch (error) {
+    logger.error(error.message);
+    return handleInternalServerError(res);
+  }
+};
+
+const updateAnotherUsersProfile = async (req, res) => {
+  try {
+    const { userId, image, name, email, role } = req.body
+
+    const reply = await userService.updateAnotherUsersProfile({ userId, image, name, email, role });
+
+    if (reply.code == 200) {
+      return handleSuccessResponse(res, 200, reply.data);
+    }
+    if (reply.code == 409) {
+      return handleErrorResponse(res, 409, "User with this email already exists, pleasy try another one");
+    }
+    else {
+      return handleInternalServerError(res);
+    }
+  }
+  catch (error) {
+    logger.error(error.message);
     return handleInternalServerError(res);
   }
 };
@@ -45,4 +85,6 @@ const updateUserProfile = async (req, res) => {
 module.exports = {
   getUserProfile,
   updateUserProfile,
+  getAllUsersProfile,
+  updateAnotherUsersProfile,
 };
