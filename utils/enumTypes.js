61042aa92d16ed93964a9981e8694a1f838b1c91
Muhammad-Ibrahim-Tayyab <ibrahim@cheetahagency.com>
2024-04-19 11:26:46 +0500

    feat: created enum for resources

---
+++ utils/enumTypes.js
@@ -0,0 +1,15 @@
+const RESOURCE_TYPES = {
+    SLIDESHOW:"slideshow", 
+    VIDEO:"video", 
+    EXIT_TICKET_TEST: "exit-ticket-test", 
+    WORKSHEET: "worksheet", 
+    QUIZ: "quiz", 
+    ASSIGNMENT: "assignment"
+}
+
+const RESOURCE_STATUS = { 
+    SHOW: "show", 
+    HIDE: "hide"
+}
+
+module.exports = {RESOURCE_TYPES, RESOURCE_STATUS}
