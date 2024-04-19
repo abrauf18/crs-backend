7b00d5b3f7aa09dc8453733bf6fbf1f4d375b98c
Muhammad-Ibrahim-Tayyab <ibrahim@cheetahagency.com>
2024-04-19 11:28:03 +0500

    feat: created resource migration and model

---
+++ migrations/20240415115433-create-resource.js
@@ -0,0 +1,60 @@
+'use strict';
+const { RESOURCE_TYPES, RESOURCE_STATUS } = require('../utils/enumTypes');
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Resources', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        defaultValue: Sequelize.UUIDV4,
+        type: Sequelize.UUID,
+      },
+      name: {
+        allowNull: false,
+        defaultValue: "",
+        type: Sequelize.STRING
+      },
+      url: {
+        allowNull: false,
+        defaultValue: "",
+        type: Sequelize.STRING
+      },
+      type: {
+        allowNull: false,
+        type: Sequelize.ENUM(
+          RESOURCE_TYPES.SLIDESHOW, 
+          RESOURCE_TYPES.VIDEO, 
+          RESOURCE_TYPES.EXIT_TICKET_TEST, 
+          RESOURCE_TYPES.WORKSHEET, 
+          RESOURCE_TYPES.QUIZ,
+          RESOURCE_TYPES.ASSIGNMENT,
+        ),
+      },
+      topic: {
+        allowNull: false,
+        defaultValue: "",
+        type: Sequelize.STRING
+      },
+      status: {
+        allowNull: false,
+        defaultValue:"show",
+        type: Sequelize.ENUM(
+          RESOURCE_STATUS.SHOW,
+          RESOURCE_STATUS.HIDE
+        ),
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Resources');
+  }
+};
