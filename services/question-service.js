4144f449066dfbe9d71f7f9872b5f4801de2fe30
Muhammad-Ibrahim-Tayyab <ibrahim@cheetahagency.com>
2024-04-25 10:40:34 +0500

    feat: create Question APIs

---
+++ services/question-service.js
@@ -0,0 +1,36 @@
+const { Sequelize } = require("sequelize");
+const { logger } = require("../Logs/logger.js");
+const { Question } = require("../models/index.js");
+const { RESOURCE_TYPES } = require("../utils/enumTypes.js");
+const question = require("../models/question.js");
+
+const createVideoQuestions = async ({ videoId, questions }) => {
+    try {
+        const createdQuestions = await Promise.all(questions.map(question => {
+            const { statement, options, totalMarks, popupTime } = question;
+            return Question.create({ videoId, statement, options, totalMarks, popupTime });
+        }));
+
+        return { code: 200, data: createdQuestions };
+    } catch (error) {
+        logger.error(error?.message || 'An error occurred while creating the video');
+        return { code: 500 };
+    }
+};
+
+const getVideoQuestions = async ({ videoId }) => {
+    try {
+        const questions = await Question.findAll({ where: { videoId: videoId } });
+
+        return { code: 200, data: questions };
+    } catch (error) {
+        console.log(error);
+        logger.error(error?.message || 'An error occurred, but no error message was provided');
+        return { code: 500 };
+    }
+};
+// Export the createVideoQuestions function
+module.exports = {
+    createVideoQuestions,
+    getVideoQuestions
+};
