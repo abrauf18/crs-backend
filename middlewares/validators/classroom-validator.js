eeffa764b64121ce8f30a4fc3a70c5a46f602690
abrauf18 <abrauf.dev@gmail.com>
2024-05-15 18:39:11 +0500

    feat: added validations to APIs

---
+++ middlewares/validators/classroom-validator.js
@@ -0,0 +1,70 @@
+const Joi = require('joi');
+const { logger } = require("../../Logs/logger");
+const { handleInternalServerError, handleErrorResponse } = require('../../utils/response-handlers');
+
+const createSchemaMiddleware = (schema, target = 'body') => async (req, res, next) => {
+    try {
+        const { error } = schema.validate(req[target]);
+        if (error) {
+            return handleErrorResponse(res, 400, error.details[0].message);
+        }
+        next();
+    } catch (error) {
+        logger.error(error?.message || 'An error occurred, but no error message was provided');
+        handleInternalServerError(res);
+    }
+};
+
+const getVideoQuestions = createSchemaMiddleware(
+    Joi.object({
+        videoid: Joi.string().guid().required(),
+        accesstoken: Joi.string().required(),
+    }).unknown(), 'headers'
+);
+
+const createClassroom = async (req, res, next) => {
+    try {
+        const schema = Joi.object({
+            teacherId: Joi.string().guid().required(),
+            name: Joi.string().required(),
+            accessToken: Joi.string().required()
+        });
+        const { error } = schema.validate(req.body);
+        if (error) {
+            return handleErrorResponse(res, 400, error.details[0].message);
+        }
+        next();
+    } catch (error) {
+        logger.error(error?.message || 'An error occurred, but no error message was provided');
+        handleInternalServerError(res);
+    }
+};
+
+const getClassroom = createSchemaMiddleware(
+    Joi.object({
+        classroomid: Joi.string().guid().required(),
+        accesstoken: Joi.string().required()
+    }).unknown(), 'headers'
+);
+
+const getAllClassroomsOfTeacher = createSchemaMiddleware(
+    Joi.object({
+        teacherid: Joi.string().guid().required(),
+        accesstoken: Joi.string().required()
+    }).unknown(), 'headers'
+);
+
+const assignStandardToClassroom = createSchemaMiddleware(
+    Joi.object({
+        classroomIds: Joi.array().items(Joi.string().guid()).required(),
+        standardId: Joi.string().guid().required(),
+        accessToken: Joi.string().required()
+    })
+);
+
+module.exports = {
+    createClassroom,
+    getClassroom,
+    getAllClassroomsOfTeacher,
+    assignStandardToClassroom,
+};
