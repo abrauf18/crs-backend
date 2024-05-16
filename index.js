7ccb9efc86ce21d59d41f89f92c4e4d52a03ac91
Wahab Afridi <121808163+ccriptdev7@users.noreply.github.com>
2024-05-16 22:25:04 +0500

    Update index.js

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.use("/school", schoolRouter);
 app.use("/resource", resourceRouter);
 
 app.get("/", (req, res) => {
-  res.send("Hello, World!");
+  res.send("Hello, World! Waahaab");
 });
 
 const port = process.env.PORT || 3000;
