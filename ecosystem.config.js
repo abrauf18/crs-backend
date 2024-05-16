b93eff6b000b41108113ff30d441dac9e34406db
Maida Afzal <maidaccript@gmail.com>
2024-05-16 20:01:46 +1000

    :bee: fixed script file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "crs-backend",
-      script: "./build/index.js",
+      script: "./index.js",
       instances: 1,
       autorestart: true,
       watch: false,
