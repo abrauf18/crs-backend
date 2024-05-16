22813638706c5dc350e149eb763bc31877ab8895
ccript <admin@ccript.com>
2024-05-17 02:12:57 +1000

    Update index.js

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ app.use("/classroom", classroomRouter);
 app.use("/dashboard", dashboardRouter);
 
 app.get("/", (req, res) => {
-  res.send("Hello, World! CRS Backend");
+  res.send("Hello, World!");
 });
 
 const port = process.env.PORT || 3000;
