905820ceb67ecf52c1768766eef55ff841846272
Maida Afzal <maidaccript@gmail.com>
2024-05-17 02:11:21 +1000

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,14 +5,7 @@ module.exports = {
       script: "./index.js",
       instances: 1,
       autorestart: true,
-      watch: false,
-      max_memory_restart: "1G",
-      env: {
-        NODE_ENV: "development",
-      },
-      env_production: {
-        NODE_ENV: "production",
-      },
+      watch: true,
     },
   ],
 };
