d2010679802e6d594a217a3db664e32bc19046e4
ccript <admin@ccript.com>
2024-05-18 00:55:31 +1000

    Update index.js

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ app.use("/classroom", classroomRouter);
 app.use("/dashboard", dashboardRouter);
 
 app.get("/", (req, res) => {
-  res.send("Hello, World! PIPELINE Chal GAEEEEEeeeee");
+  res.send("Hello, World! PIPELINE Working");
 });
 
 const port = process.env.PORT || 3000;
