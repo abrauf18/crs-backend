a4a85fff93947300539d504d8fabcfbf6b600a2f
Muhammad-Ibrahim-Tayyab <ibrahim@cheetahagency.com>
2024-05-27 15:38:34 +0500

    feat: create APIs for get and store student video after it has been watched partially or fully

--- services/student-service.js
+++ services/student-service.js
@@ -1,8 +1,25 @@
 const { Sequelize } = require("sequelize");
 const { logger } = require("../Logs/logger.js");
-const { Classroom, Standard, ClassroomCourses, ClassroomStudent, User, DailyUpload, Resource, Video } = require("../models/index.js");
+const { Classroom, Standard, ClassroomCourses, ClassroomStudent, User, DailyUpload, Resource, Video, VideoTracking, Question,VideoQuestionAnswer } = require("../models/index.js");
 const { CLASSROOM_STATUS } = require("../utils/enumTypes.js");
-const video = require("../models/video.js");
+
+function timeToSeconds(time) {
+    const [hours, minutes, seconds] = time.split(':').map(Number);
+    return hours * 3600 + minutes * 60 + seconds;
+}
+
+function compareTimes(time1, time2) {
+    const time1Seconds = timeToSeconds(time1);
+    const time2Seconds = timeToSeconds(time2);
+
+    if (time1Seconds > time2Seconds) {
+        return 1; // time1 is greater than time2
+    } else if (time1Seconds < time2Seconds) {
+        return -1; // time1 is less than time2
+    } else {
+        return 0; // time1 is equal to time2
+    }
+}
 
 const getStudentCurrentStandards = async ({ studentId }) => {
     try {
@@ -50,9 +67,9 @@ const getStudentCurrentStandards = async ({ studentId }) => {
             }, 0);
         
             return {
-                standardId: course.standard.id,
+                id: course.standard.id,
                 courseLength: course.standard.courseLength,
-                standardName: course.standard.name,
+                name: course.standard.name,
                 standardDescription: course.standard.description,
                 totalVideoUploads: totalVideoUploads,
                 totalNonVideoUploads: course.standard.dailyUploads.length - totalVideoUploads
@@ -129,7 +146,133 @@ const getStandardResources = async ({ standardId }) => {
     }
 };
 
+const getStudentVideo = async ({ videoId, studentId }) => {
+    try {
+        const student = await User.findByPk(studentId);
+        if (!student) {
+            return { code: 404, message: 'Student not found'};
+        }
+
+        const watchedVideo = await VideoTracking.findOne({
+            where: {
+                videoId: videoId,
+                studentId: studentId
+            }
+        });
+
+        if (watchedVideo) {
+            const video = await Video.findOne({
+                where: { id: videoId },
+                include: [{
+                    model: Resource,
+                    as: 'resource',
+                    attributes: ['name', 'url'],
+                }, {
+                    model: Question,
+                    as: 'questions',
+                    order: [['popUpTime', 'ASC']],
+                    include: [{
+                        model: VideoQuestionAnswer,
+                        as: 'answers',
+                        where: { userId: studentId },
+                    }]
+                }],
+                group: ['Video.id', 'resource.id', 'questions.id', 'questions.answers.id'],
+            });
+
+            if (!video) {
+                return { code: 404, message: 'Video not found'};
+            }
+
+            const transformedQuestions = video.questions.map(question => {
+                const { answers, createdAt, updatedAt, videoId, ...questionData } = question.get({ plain: true });
+                const transformedAnswers = answers.map(answer => {
+                    const { createdAt, updatedAt, userId, questionId, ...answerData } = answer;
+                    return answerData;
+                });
+                return {
+                    ...questionData,
+                    answer: transformedAnswers
+                };
+            });
+
+            const { resource, questions, topics, createdAt, updatedAt, ...videoData } = video.get({ plain: true });
+
+            return {
+                code: 200,
+                data: {
+                    video: { ...videoData, name: resource.name, videoUrl: resource.url, questions: transformedQuestions, topics }
+                }
+            };
+            
+        } else {
+            const video = await Video.findOne({
+                where: { id: videoId },
+                include: [{
+                    model: Resource,
+                    as: 'resource',
+                    attributes: ['name', 'url'],
+                }, {
+                    model: Question,
+                    as: 'questions',
+                    order: [['popUpTime', 'ASC']],
+                }],
+                group: ['Video.id', 'resource.id', 'questions.id'],
+            });
+    
+            if (!video) {
+                return { code: 404, message: 'Video not found'};
+            }
+    
+            const { resource, questions, topics, ...videoData } = video.get({ plain: true });
+
+            return {
+                code: 200,
+                data: {
+                    video: { ...videoData, name: resource.name, videoUrl: resource.url, questions, topics }
+                }
+            };
+        }
+    } catch (error) {
+        console.log('\n\n\n\n', error)
+        logger.error(error?.message || 'An error occurred while fetching the video');
+        return { code: 500 };
+    }
+}
+
+const storeStudentVideo = async ({ videoId, studentId, last_seen_time }) => {
+    try {
+        const video = await Video.findByPk(videoId);
+        if (!video) {
+            return { code: 404, message: 'Video not found'};
+        }
+
+        const student = await User.findByPk(studentId);
+        if (!student) {
+            return { code: 404, message: 'Student not found'};
+        }
+
+        if (compareTimes(last_seen_time, video.duration) > 0) {
+            return { code: 400 };
+        }
+
+        const videotracking = await VideoTracking.create({
+            videoId: videoId,
+            studentId: studentId,
+            last_seen_time: last_seen_time,
+        });
+
+        return { code: 200, data: videotracking};
+    } catch (error) {
+        console.log('\n\n\n\n', error)
+        logger.error(error?.message || 'An error occurred while storing the video');
+        return { code: 500 };
+    }
+}
+
 module.exports = {
     getStudentCurrentStandards,
-    getStandardResources
+    getStandardResources,
+    getStudentVideo,
+    storeStudentVideo,
 };
