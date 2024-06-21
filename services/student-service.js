4531225ad8252e6c803163d9e42dcf283385820c
Muhammad-Ibrahim-Tayyab <ibrahim@cheetahagency.com>
2024-06-21 19:46:26 +0500

    faet: create routes for stundet modult in teacher view

--- services/student-service.js
+++ services/student-service.js
@@ -1148,6 +1148,379 @@ const getStudentProfileSummarizedStandards = async ({ studentId }) => {
     }
 }
 
+const getSummarizedStudentStandardsForTeacher = async ({ studentId }) => {
+    try {
+        const student = await User.findByPk(studentId);
+        if (!student) {
+            return { code: 404, message: 'Student not found'};
+        }
+
+        const data = await ClassroomStudent.findOne({
+            where: {
+                studentId: studentId
+            },
+            attributes: [ "id" ],
+            include: [ 
+                {
+                    model: Classroom,
+                    as: 'classroom',
+                    where: { status: CLASSROOM_STATUS.ACTIVE },
+                    attributes: [ "id", "name" ],
+                    include: [{
+                        model: ClassroomCourses,
+                        as: 'classroomCourses',
+                        attributes: [ "id" ],
+                        include: [{
+                            model: Standard,
+                            as: 'standard',
+                            attributes: [ "id", "name" ],
+                            include: [{ 
+                                model: DailyUpload, 
+                                as: 'dailyUploads',
+                                attributes: ['id', 'accessDate', 'weightage'],
+                                where: {
+                                    weightage: {
+                                        [Op.gt]: 0
+                                    }
+                                },
+                                required: true,
+                                separate: true,
+                                include: [{
+                                    model: Resource,
+                                    as: 'resource',
+                                    attributes: ['id', 'name', 'type', 'topic', 'url'],
+                                    include: [
+                                        {
+                                            model: Video,
+                                            as: 'video',
+                                            attributes: ['id'],
+                                            include: [{
+                                                separate: true,
+                                                model: Question,
+                                                as: 'questions',
+                                                required: false,
+                                                attributes: ['id', 'totalMarks'],
+                                                include: [{
+                                                    separate: true,
+                                                    model: VideoQuestionAnswer,
+                                                    as: 'answers',
+                                                    where: { userId: studentId },
+                                                    required: false,
+                                                    attributes: ['obtainedMarks']
+                                                }]
+                                            }]
+                                        },
+                                        {
+                                            model: AssessmentResourcesDetail,
+                                            as: 'AssessmentResourcesDetail',
+                                            attributes: ['id', 'totalMarks', 'deadline'],
+                                            include: [{
+                                                separate: true,
+                                                model: AssessmentAnswer,
+                                                as: 'assessmentAnswers',
+                                                where: { userId: studentId },
+                                                attributes: ['obtainedMarks', 'answerURL'],
+                                                required: false,
+                                            }]
+                                        }
+                                    ]
+                                }]
+                            }]
+                        }],
+                    }],
+                }
+            ],
+        });
+
+        // Current date for comparison
+        const today = new Date();
+
+        const result = await data?.get({plain: true});
+
+        if (!result || !result.classroom || !result.classroom.classroomCourses) {
+            return {
+                code: 200,
+                data: [],
+            };
+        }
+
+        const transformedData = data?.classroom?.classroomCourses?.map(course => {
+            const standard = course?.standard;
+            let totalWeightage = 0;
+            let obtainedWeightage = 0;
+    
+            standard?.dailyUploads?.forEach(upload => {
+                let totalObtainedMarks = 0;
+                let totalPossibleMarks = 0;
+                const accessDate = new Date(upload.accessDate);
+    
+                // Only consider uploads with accessDate < today
+                if (accessDate < today) {
+                    totalWeightage += upload.weightage;
+    
+                    // Process resource's video questions
+                    if (upload.resource.video) {
+                        totalPossibleMarks = upload.resource.video?.questions?.reduce((total, question) => {
+                            return total + question.totalMarks;
+                        }, 0);
+                        upload.resource.video.questions.forEach(question => {
+                            question?.answers?.forEach(answer => {
+                                totalObtainedMarks += answer.obtainedMarks === -1 ? 0 : answer.obtainedMarks;
+                            });
+                        });
+                    }
+    
+                    // Process resource's assessment answers
+                    if (upload.resource.AssessmentResourcesDetail) {
+                        upload.resource.AssessmentResourcesDetail.assessmentAnswers.forEach(answer => {
+                            totalObtainedMarks += answer?.obtainedMarks === -1 ? 0 : answer?.obtainedMarks;
+                        });
+                        totalPossibleMarks += upload.resource.AssessmentResourcesDetail?.totalMarks || 0;
+                    }
+                    obtainedWeightage += totalPossibleMarks > 0 ? (totalObtainedMarks / totalPossibleMarks) * upload.weightage : upload.weightage;
+                }
+            })
+            return {
+                standardId: standard.id,
+                standardName: standard.name,
+                totalWeightage: totalWeightage,
+                obtainedWeightage: obtainedWeightage
+            };
+        })?.filter(entry => entry.totalWeightage > 0);
+
+        // Calculate average total weightage
+        const totalTotalWeightage = transformedData?.reduce((total, entry) => {
+            return total + entry.totalWeightage;
+        }, 0);
+        const averageTotalWeightage = transformedData?.length > 0 ? totalTotalWeightage / transformedData.length : 0;
+        
+        // Calculate average obtained weightage
+        const totalObtainedWeightage = transformedData?.reduce((total, entry) => {
+            return total + entry.obtainedWeightage;
+        }, 0);
+        const averageObtainedWeightage = transformedData?.length > 0 ? totalObtainedWeightage / transformedData.length : 0;
+
+        return {
+            code: 200,
+            data: {
+                summarizedStandardResults: transformedData,
+                averageTotalWeightage: averageTotalWeightage,
+                averageObtainedWeightage: averageObtainedWeightage,
+            }
+        };
+    } catch (error) {
+        console.log('\n\n\n\n', error)
+        logger.error(error?.message || 'An error occurred while fetching the saved videos');
+        return { code: 500 };
+    }
+}
+
+const getSummarizedStudentForTeacher = async ({ studentId }) => {
+    try {
+        const student = await User.findByPk(studentId);
+        if (!student) {
+            return { code: 404, message: 'Student not found'};
+        }
+
+        const data = await ClassroomStudent.findOne({
+            where: {
+                studentId: studentId
+            },
+            attributes: [ "id" ],
+            include: [ 
+                {
+                    model: Classroom,
+                    as: 'classroom',
+                    where: { status: CLASSROOM_STATUS.ACTIVE },
+                    attributes: [ "id", "name" ],
+                    include: [{
+                        model: ClassroomCourses,
+                        as: 'classroomCourses',
+                        attributes: [ "id" ],
+                        include: [{
+                            model: Standard,
+                            as: 'standard',
+                            attributes: [ "id", "name" ],
+                            include: [{ 
+                                model: DailyUpload, 
+                                as: 'dailyUploads',
+                                attributes: ['id', 'accessDate', 'weightage'],
+                                where: {
+                                    weightage: {
+                                        [Op.gt]: 0
+                                    }
+                                },
+                                required: true,
+                                separate: true,
+                                include: [{
+                                    model: Resource,
+                                    as: 'resource',
+                                    attributes: ['id', 'name', 'type', 'topic', 'url'],
+                                    include: [
+                                        {
+                                            model: Video,
+                                            as: 'video',
+                                            attributes: ['id'],
+                                            include: [{
+                                                separate: true,
+                                                model: Question,
+                                                as: 'questions',
+                                                required: false,
+                                                attributes: ['id', 'totalMarks'],
+                                                include: [{
+                                                    separate: true,
+                                                    model: VideoQuestionAnswer,
+                                                    as: 'answers',
+                                                    where: { userId: studentId },
+                                                    required: false,
+                                                    attributes: ['obtainedMarks']
+                                                }]
+                                            }]
+                                        },
+                                        {
+                                            model: AssessmentResourcesDetail,
+                                            as: 'AssessmentResourcesDetail',
+                                            attributes: ['id', 'totalMarks', 'deadline'],
+                                            include: [{
+                                                separate: true,
+                                                model: AssessmentAnswer,
+                                                as: 'assessmentAnswers',
+                                                where: { userId: studentId },
+                                                attributes: ['obtainedMarks', 'answerURL'],
+                                                required: false,
+                                            }]
+                                        }
+                                    ]
+                                }]
+                            }]
+                        }],
+                    }],
+                }
+            ],
+        });
+
+        // Current date for comparison
+        const today = new Date();
+
+        const plainStudent = await student.get({plain: true});
+        const result = await data?.get({plain: true});
+
+        if (!result || !result.classroom || !result.classroom.classroomCourses) {
+            return {
+                code: 200,
+                data: [],
+            };
+        }
+
+        const transformedData = data?.classroom?.classroomCourses?.map(course => {
+            const standard = course?.standard;
+            let totalWeightage = 0;
+            let obtainedWeightage = 0;
+    
+            standard?.dailyUploads?.forEach(upload => {
+                let totalObtainedMarks = 0;
+                let totalPossibleMarks = 0;
+                const accessDate = new Date(upload.accessDate);
+    
+                // Only consider uploads with accessDate < today
+                if (accessDate < today) {
+                    totalWeightage += upload.weightage;
+    
+                    // Process resource's video questions
+                    if (upload.resource.video) {
+                        totalPossibleMarks = upload.resource.video?.questions?.reduce((total, question) => {
+                            return total + question.totalMarks;
+                        }, 0);
+                        upload.resource.video.questions.forEach(question => {
+                            question?.answers?.forEach(answer => {
+                                totalObtainedMarks += answer.obtainedMarks === -1 ? 0 : answer.obtainedMarks;
+                            });
+                        });
+                    }
+    
+                    // Process resource's assessment answers
+                    if (upload.resource.AssessmentResourcesDetail) {
+                        upload.resource.AssessmentResourcesDetail.assessmentAnswers.forEach(answer => {
+                            totalObtainedMarks += answer?.obtainedMarks === -1 ? 0 : answer?.obtainedMarks;
+                        });
+                        totalPossibleMarks += upload.resource.AssessmentResourcesDetail?.totalMarks || 0;
+                    }
+                    obtainedWeightage += totalPossibleMarks > 0 ? (totalObtainedMarks / totalPossibleMarks) * upload.weightage : upload.weightage;
+                }
+            })
+            return {
+                standardId: standard.id,
+                standardName: standard.name,
+                totalWeightage: totalWeightage,
+                obtainedWeightage: obtainedWeightage
+            };
+        })?.filter(entry => entry.totalWeightage > 0);
+
+        // Calculate average total weightage
+        const totalTotalWeightage = transformedData?.reduce((total, entry) => {
+            return total + entry.totalWeightage;
+        }, 0);
+        const averageTotalWeightage = transformedData?.length > 0 ? totalTotalWeightage / transformedData.length : 0;
+        
+        // Calculate average obtained weightage
+        const totalObtainedWeightage = transformedData?.reduce((total, entry) => {
+            return total + entry.obtainedWeightage;
+        }, 0);
+        const averageObtainedWeightage = transformedData?.length > 0 ? totalObtainedWeightage / transformedData.length : 0;
+        
+        const classroomName = result.classroom.name;
+
+        const { name, email, image } = plainStudent;
+
+        return {
+            code: 200,
+            data: {
+                student: { 
+                    name: name, 
+                    email: email, 
+                    image: image, 
+                    classroomName: classroomName,
+                    averageTotalWeightage: averageTotalWeightage,
+                    averageObtainedWeightage: averageObtainedWeightage,
+                },
+                summarizedStandardResults: transformedData,
+            }
+        };
+    } catch (error) {
+        console.log('\n\n\n\n', error)
+        logger.error(error?.message || 'An error occurred while fetching the saved videos');
+        return { code: 500 };
+    }
+}
+
+const getStudentNameEmailForTeacher = async ({ studentId }) => {
+    try {
+        const student = await User.findByPk(studentId);
+        if (!student) {
+            return { code: 404, message: 'Student not found'};
+        }
+
+        const plainStudent = await student.get({plain: true});
+
+        const { name, email } = plainStudent;
+
+        return {
+            code: 200,
+            data: {
+                student: { 
+                    name: name, 
+                    email: email, 
+                }
+            }
+        };
+    } catch (error) {
+        console.log('\n\n\n\n', error)
+        logger.error(error?.message || 'An error occurred while fetching the saved videos');
+        return { code: 500 };
+    }
+}
+
+
 module.exports = {
     getStudentCurrentStandards,
     getStudentVideo,
@@ -1159,5 +1532,8 @@ module.exports = {
     getSavedVideos,
     getStandardsResourcesAndCount,
     getStudentProfileStandardResults,
-    getStudentProfileSummarizedStandards
+    getStudentProfileSummarizedStandards,
+    getSummarizedStudentStandardsForTeacher,
+    getSummarizedStudentForTeacher,
+    getStudentNameEmailForTeacher
 };
