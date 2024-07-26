ad92c6ac9485f3d42723c8181a418a8bf4769b04
abrauf18 <abrauf.dev@gmail.com>
2024-07-26 16:18:25 +0500

    feat: create enrollments table to store standard results

---
+++ models/enrollment.js
@@ -0,0 +1,56 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Enrollment extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      models.Enrollment.belongsTo(models.Classroom, {
+        foreignKey: 'classroomId',
+      });
+      models.Enrollment.belongsTo(models.Standard, {
+        foreignKey: 'standardId',
+      });
+      models.Enrollment.belongsTo(models.User, {
+        foreignKey: 'studentId',
+      });
+    }
+  }
+  Enrollment.init({
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+      allowNull: false,
+    },
+    classroomId: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      defaultValue:'',
+    },
+    standardId: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      defaultValue:'',
+    },
+    studentId: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      defaultValue:'',
+    },
+    result: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      defaultValue:0,
+    },
+  }, {
+    sequelize,
+    modelName: 'Enrollment',
+  });
+  return Enrollment;
+};
